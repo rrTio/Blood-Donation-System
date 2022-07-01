3f78cf915698f3dfa5be69c56a0b3001481aaf34
rrTio <tio.rozsraven@ue.edu.ph>
2022-07-01 21:50:28 +0800

    Created Registration for Donors, constant tables for registration values

--- database/connection.php
+++ database/connection.php
@@ -15,6 +15,7 @@ $createDonorTable = "CREATE TABLE IF NOT EXISTS `donortable`(
     `firstName` TEXT,
     `middleName` TEXT,
     `lastName` TEXT,
+    `fullName` TEXT,
     `age` VARCHAR(3),
     `gender` TEXT,
     `donationDate` DATE,
@@ -25,10 +26,40 @@ $createDonorTable = "CREATE TABLE IF NOT EXISTS `donortable`(
     `donationVenue` TEXT
 );";
 
-$createBloodTypes = "CREATE TABLE IF NOT EXISTS `bloodTypes`(`bloodType` TEXT);";
-$insertBloodTypes = "INSERT INTO bloodTypes(bloodType) VALUES ('A+'), ('A-'), ('B+'), ('B-'), ('O+'), ('O-'), ('AB+'), ('AB-');";
+$createPatient = "CREATE TABLE IF NOT EXISTS `patientsTable`(
+    `firstName` TEXT,
+    `middleName` TEXT,
+    `lastName` TEXT,
+    `fullName` TEXT,
+    `age` VARCHAR(3),
+    `patientAddress` TEXT,
+    `gender` TEXT
+);";
+
+$createBloodTypes = "CREATE TABLE IF NOT EXISTS `bloodTypesTable`(`bloodType` TEXT UNIQUE);";
+$createOrganization = "CREATE TABLE IF NOT EXISTS  `organizationsTable`(`organization` TEXT UNIQUE);";
+$createPositions = "CREATE TABLE IF NOT EXISTS `positionsTable`(`positions` TEXT UNIQUE);";
+$createMonths = "CREATE TABLE IF NOT EXISTS `birthMonths`(`birthmonths` TEXT UNIQUE);";
+$createDays = "CREATE TABLE IF NOT EXISTS `birthDays` (`birthdays` VARCHAR(2) UNIQUE);";
+
+$insertBloodTypes = "INSERT IGNORE INTO bloodTypesTable(bloodType) VALUES ('A+'), ('A-'), ('B+'), ('B-'), ('O+'), ('O-'), ('AB+'), ('AB-');";
+$insertOrganization = "INSERT IGNORE INTO organizationsTable(organization) VALUES ('Dagupan City'), ('Alaminos City'), ('San Carlos City');" ;
+$insertPosition = "INSERT IGNORE INTO positionsTable(positions) VALUES ('Admin'), ('Staff');";
+$insertMonths = "INSERT IGNORE INTO birthMonths (birthmonths) VALUES ('January'),('February'),('March'),('April'),('May'),('June'),('July'),('August'),('September'),('October'),('November'),('December');";
+$insertDays = "INSERT IGNORE INTO birthDays (birthdays) VALUES ('01'),('02'),('03'),('04'),('05'),('06'),('07'),('08'),('09'),('10'),('11'),('12'),('13'),('14'),('15'),('16'),('17'),('18'),('19'),('20'),('21'),('22'),('23'),('24'),('25'),('26'),('27'),('28'),('29'),('30'),('31');";
 
 mysqli_query($conn, $createDonorTable);
+mysqli_query($conn, $createPatient);
+
 mysqli_query($conn, $createBloodTypes);
+mysqli_query($conn, $createOrganization);
+mysqli_query($conn, $createPositions);
+mysqli_query($conn, $createMonths);
+mysqli_query($conn, $createDays);
+
 mysqli_query($conn, $insertBloodTypes);
+mysqli_query($conn, $insertOrganization);
+mysqli_query($conn, $insertPosition);
+mysqli_query($conn, $insertMonths);
+mysqli_query($conn, $insertDays);
 ?>
